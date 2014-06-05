c71922fc9735e8395aafee938b0264a113566caa
daniel865 <daniel865live@gmail.com>
2014-06-05 15:48:24 -0500

    new commit

--- src/java/com/webjudo/servlets/ServletDeportista.java
+++ src/java/com/webjudo/servlets/ServletDeportista.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package com.webjudo.servlets;
 
 import com.webjudo.dao.Conexion;
@@ -40,14 +39,13 @@ public class ServletDeportista extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
-        
+
         String accion = request.getParameter("accion");
-        accion = accion==null ? "Inicio" : accion;
-        
-        
-        if ( "Inicio".equals(accion) ){
+        accion = accion == null ? "Inicio" : accion;
+
+        if ("Inicio".equals(accion)) {
             request.getRequestDispatcher("GestionarDeportista.jsp").forward(request, response);
-        }else if ( "Guardar".equals(accion) ){
+        } else if ("Guardar".equals(accion)) {
             DeportistaDAO deportistaDAO = new DeportistaDAO(new Conexion("dbapolijudo", "polijic", "jdbc:oracle:thin:@localhost:1521:XE"));
             SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
             String nro_identificacion = request.getParameter("nro_identificacion");
@@ -62,7 +60,7 @@ public class ServletDeportista extends HttpServlet {
             String telefono = request.getParameter("telefono");
             String celular = request.getParameter("celular");
             String cinturon_actual = request.getParameter("cinturon_actual");
-            String cinturon_proximo = request.getParameter("cinturon_proximo");  
+            String cinturon_proximo = request.getParameter("cinturon_proximo");
             String fecha_nac = request.getParameter("fecha_nac");
             Date fecha_nacimiento = null;
             try {
@@ -72,8 +70,8 @@ public class ServletDeportista extends HttpServlet {
                 Logger.getLogger(ServletDeportista.class.getName()).log(Level.SEVERE, null, ex);
             }
             try {
-                deportistaDAO.crearDeportista(new Deportista(Integer.parseInt(nro_identificacion), nombres, apellido2, apellido2, fecha_nacimiento, programa_academico, Integer.parseInt(semestre), 
-                        Integer.parseInt(grupo), medallas, Integer.parseInt(cinturon_actual), Integer.parseInt(cinturon_proximo), direccion, Integer.parseInt(telefono), Integer.parseInt(celular) ));
+                deportistaDAO.crearDeportista(new Deportista(Integer.parseInt(nro_identificacion), nombres, apellido2, apellido2, fecha_nacimiento, programa_academico, Integer.parseInt(semestre),
+                        Integer.parseInt(grupo), medallas, Integer.parseInt(cinturon_actual), Integer.parseInt(cinturon_proximo), direccion, Integer.parseInt(telefono), Integer.parseInt(celular)));
             } catch (Exception e) {
                 Logger.getLogger(ServletDeportista.class.getName()).log(Level.SEVERE, null, e);
                 request.setAttribute("mensaje", e.getMessage());
@@ -90,21 +88,87 @@ public class ServletDeportista extends HttpServlet {
                 request.setAttribute("celular", celular);
                 request.setAttribute("cinturon_actual", cinturon_actual);
                 request.setAttribute("cinturon_proximo", cinturon_proximo);
-                request.setAttribute("fecha_nac", fecha_nac);  
+                request.setAttribute("fecha_nac", fecha_nac);
             }
             request.getRequestDispatcher("GestionarDeportista.jsp").forward(request, response);
-        }else if ( "Consultar".equals(accion) ){
+        } else if ("Consultar".equals(accion)) {
             DeportistaDAO deportistaDAO = new DeportistaDAO(new Conexion("dbapolijudo", "polijic", "jdbc:oracle:thin:@localhost:1521:XE"));
-            
-        }else if ( "Modificar".equals(accion) ){
-            
-        }else if ( "Eliminar".equals(accion) ){
-            
+            String id = request.getParameter("buscar_dep");
+            Deportista deportista;
+            try {
+                deportista = deportistaDAO.buscarDeportista(Integer.parseInt(id));
+                request.setAttribute("mensaje", "El deportista fue encontrado");
+                request.setAttribute("nro_identificacion", deportista.getNro_identificacion());
+                request.setAttribute("nombres", deportista.getNombre());
+                request.setAttribute("apellido1", deportista.getPrimer_apellido());
+                request.setAttribute("apellido2", deportista.getSegundo_apellido());
+                request.setAttribute("programa_academico", deportista.getPrograma_academico());
+                request.setAttribute("semestre", deportista.getSemestre());
+                request.setAttribute("grupo", deportista.getGrupo());
+                request.setAttribute("medallas", deportista.getMedallas());
+                request.setAttribute("direccion", deportista.getDireccion());
+                request.setAttribute("telefono", deportista.getTelefono());
+                request.setAttribute("celular", deportista.getCelular());
+                request.setAttribute("cinturon_actual", deportista.getCinturon_actual());
+                request.setAttribute("cinturon_proximo", deportista.getCinturon_proximo());
+                request.setAttribute("fecha_nac", deportista.getFecha_nacimiento());
+                request.getRequestDispatcher("GestionarDeportista.jsp").forward(request, response);
+            } catch (Exception e) {
+                Logger.getLogger(ServletDeportista.class.getName()).log(Level.SEVERE, null, e);
+                request.setAttribute("mensaje", "No se pudo encontrar el deportista");
+                request.getRequestDispatcher("GestionarDeportista.jsp").forward(request, response);
+            }
+        } else if ("Modificar".equals(accion)) {
+            DeportistaDAO deportistaDAO = new DeportistaDAO(new Conexion("dbapolijudo", "polijic", "jdbc:oracle:thin:@localhost:1521:XE"));
+            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+            String nro_identificacion = request.getParameter("nro_identificacion");
+            String nombres = request.getParameter("nombres");
+            String apellido1 = request.getParameter("apellido1");
+            String apellido2 = request.getParameter("apellido2");
+            String programa_academico = request.getParameter("programa_academico");
+            String semestre = request.getParameter("semestre");
+            String grupo = request.getParameter("grupo");
+            String medallas = request.getParameter("medallas");
+            String direccion = request.getParameter("direccion");
+            String telefono = request.getParameter("telefono");
+            String celular = request.getParameter("celular");
+            String cinturon_actual = request.getParameter("cinturon_actual");
+            String cinturon_proximo = request.getParameter("cinturon_proximo");
+            String fecha_nac = request.getParameter("fecha_nac");
+            Date fecha_nacimiento = null;
+            try {
+                java.util.Date fecha_naci = formatter.parse(fecha_nac);
+                fecha_nacimiento = new Date(fecha_naci.getTime());
+            } catch (ParseException ex) {
+                Logger.getLogger(ServletDeportista.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            try {
+                deportistaDAO.modificarDeportista(new Deportista(Integer.parseInt(nro_identificacion), nombres, apellido2, apellido2, fecha_nacimiento, programa_academico, Integer.parseInt(semestre),
+                        Integer.parseInt(grupo), medallas, Integer.parseInt(cinturon_actual), Integer.parseInt(cinturon_proximo), direccion, Integer.parseInt(telefono), Integer.parseInt(celular)));
+            } catch (Exception e) {
+                Logger.getLogger(ServletDeportista.class.getName()).log(Level.SEVERE, null, e);
+                request.setAttribute("mensaje", e.getMessage());
+                request.setAttribute("nro_identificacion", nro_identificacion);
+                request.setAttribute("nombres", nombres);
+                request.setAttribute("apellido1", apellido1);
+                request.setAttribute("apellido2", apellido2);
+                request.setAttribute("programa_academico", programa_academico);
+                request.setAttribute("semestre", semestre);
+                request.setAttribute("grupo", grupo);
+                request.setAttribute("medallas", medallas);
+                request.setAttribute("direccion", direccion);
+                request.setAttribute("telefono", telefono);
+                request.setAttribute("celular", celular);
+                request.setAttribute("cinturon_actual", cinturon_actual);
+                request.setAttribute("cinturon_proximo", cinturon_proximo);
+                request.setAttribute("fecha_nac", fecha_nac);
+            }
+            request.getRequestDispatcher("GestionarDeportista.jsp").forward(request, response);
+        } else if ("Eliminar".equals(accion)) {
+
         }
-        
-        
+
     }
-    
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
